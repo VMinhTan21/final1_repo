c2cd3f1d8fadce2319ba0ae22b6ccaed89b4bebc
VMinhTan <vienminhtanmd@gmail.com>
2023-06-24 15:42:55 +0700

    update list order

--- src/components/TestDataBase.jsx
+++ src/components/TestDataBase.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import ViewOrder from "./ViewOrder";
 import { Container } from "react-bootstrap";
 
 import HomeNavbar from "./HomeNavbar";
+import { ToastContainer, toast } from "react-toastify";
 
-const TestDataBase = () => {
-  return (
-    <div>
-      <Container>
-        <HomeNavbar />
-        <h2>LIST ORDER</h2>
-        <br />
-        <ViewOrder />
+class TestDataBase extends Component {
+  componentDidMount() {
+    const channel = new BroadcastChannel("6B29FC40-CA47-1067-B31D-00DD010662DA");
 
-      </Container>
-    </div>
-  );
-};
+    const handleMessage = (event) => {
+      toast.success("NEW ORDER FROM CLIENT", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    };
+
+    channel.addEventListener("message", handleMessage);
+
+    this.channel = channel;
+  }
+
+  componentWillUnmount() {
+    if (this.channel) {
+      this.channel.removeEventListener("message", this.handleMessage);
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Container>
+          <HomeNavbar />
+          <h2>LIST ORDER</h2>
+          <br />
+          <ViewOrder />
+          <ToastContainer />
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default TestDataBase;
